1d9d71b435f8205fbb78fda49adbbba1451805a0
Dan Alwood <dalwood@yahoo.com>
2023-11-21 07:20:42 -0600

    Initial commit of viable classes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -0,0 +1,359 @@
+/* Copyright (c) 2022 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+import java.util.List;
+
+/**
+ * Instead of each Op Mode class redefining the robot's hardware resources within its implementation,
+ * This RobotHardware class has a given robot's component resources defined and set up all in one place.
+ * It also has convenience methods like driveRobot(), setArmPower(), setHandPosition(), etc. that work for that robot.
+ *
+ * Where possible, the actual hardware objects are "abstracted" (or hidden) so the OpMode code just makes calls into the class,
+ * rather than accessing the internal hardware directly. This is why the objects are declared "private".
+ */
+public class RobotHardware {
+
+    /* Declare OpMode members. */
+    private LinearOpMode myOpMode = null;   // gain access to methods in the calling OpMode.
+
+    // Define all the HardwareDevices (Motors, Servos, etc.). Make them private so they can't be accessed externally.
+    private DcMotor leftFrontWheel;
+    private DcMotor rightFrontWheel;
+    private DcMotor leftRearWheel;
+    private DcMotor rightRearWheel;
+    private WebcamName webCam;
+
+    // Define other HardwareDevices as needed.
+    private DcMotor armMotor;
+    private Servo   leftHand;
+    private Servo   rightHand;
+    private TfodProcessor tfod;
+    private VisionPortal visionPortal;
+
+    // Define Drive constants.  Make them public so they CAN be used by the calling OpMode
+    static final double COUNTS_PER_MOTOR_REV = 560;
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DEFAULT_WHEEL_MOTOR_SPEED = .4;
+    public static final double MID_SERVO       =  0.5 ;
+    public static final double HAND_SPEED      =  0.02 ;  // sets rate to move servo
+    public static final double ARM_UP_POWER    =  0.45 ;
+    public static final double ARM_DOWN_POWER  = -0.45 ;
+
+    // The one and only constructor requires a reference to an OpMode.
+    public RobotHardware(LinearOpMode opmode) {
+        myOpMode = opmode;
+    }
+
+    /**
+     * Call init() to initialize all the robot's hardware.
+     */
+    public void init() {
+        initWheelMotors();
+        initServos();
+        initTfod();
+        initVisionPortal();
+
+        myOpMode.telemetry.addData(">", "Hardware Initialized");
+        myOpMode.telemetry.update();
+    }
+
+    /**
+     * Call shutDown() to stop and close all the robot's hardware.
+     */
+    public void shutDown() {
+        tfod.shutdown();
+        visionPortal.close();
+    }
+    /**
+     * Initialize all the wheel motors.
+     * This method must be called ONCE when the OpMode is initialized.
+     *
+     * All of the hardware devices are accessed via the hardware map, and initialized.
+     */
+    private void initWheelMotors()    {
+        // Define and Initialize Motors (note: need to use reference to actual OpMode).
+        leftFrontWheel  = myOpMode.hardwareMap.get(DcMotor.class, "LFront");
+        rightFrontWheel = myOpMode.hardwareMap.get(DcMotor.class, "RFront");
+        leftRearWheel = myOpMode.hardwareMap.get(DcMotor.class, "LRear");
+        rightRearWheel = myOpMode.hardwareMap.get(DcMotor.class, "RRear");
+
+        armMotor   = myOpMode.hardwareMap.get(DcMotor.class, "arm");
+
+        // To drive forward, most robots need the motors on one side to be reversed, because the axles point in opposite directions.
+        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
+        leftFrontWheel.setDirection(DcMotor.Direction.FORWARD);
+        leftRearWheel.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontWheel.setDirection(DcMotor.Direction.REVERSE);
+        rightRearWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        // If there are encoders connected, switch to RUN_USING_ENCODER mode for greater accuracy
+        leftFrontWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRearWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRearWheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Set wheel motors to not resist turning when motor is stopped.
+        leftFrontWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rightFrontWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        leftRearWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rightRearWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+    }
+
+    /**
+     * Initialize all servos.
+     */
+    private void initServos() {
+        // Define and initialize ALL installed servos.
+        leftHand = myOpMode.hardwareMap.get(Servo.class, "left_hand");
+        rightHand = myOpMode.hardwareMap.get(Servo.class, "right_hand");
+        leftHand.setPosition(MID_SERVO);
+        rightHand.setPosition(MID_SERVO);
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection processor.
+     */
+    private void initTfod() {
+        // Create the TensorFlow processor by using its Builder.
+        tfod = new TfodProcessor.Builder()
+                // Call various setters on the Builder.
+                // Use setModelAssetName() if the TF Model is built in as an asset.
+                // Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
+                //.setModelAssetName(TFOD_MODEL_ASSET)
+                //.setModelFileName(TFOD_MODEL_FILE)
+                //.setModelLabels(LABELS)
+                //.setIsModelTensorFlow2(true)
+                //.setIsModelQuantized(true)
+                //.setModelInputSize(300)
+                //.setModelAspectRatio(16.0 / 9.0)
+
+                //Finally, call build() to return a TfodProcessor.
+                .build();
+
+        // Set confidence threshold for TFOD recognitions, at any time.
+        //tfod.setMinResultConfidence(0.75f);
+
+        // Disable or re-enable the TFOD processor at any time.
+        //visionPortal.setProcessorEnabled(tfod, true);
+    }
+
+    /**
+     * Initialize the VisionPortal. tfod is expected to be initialized before calling this method.
+     */
+    private void initVisionPortal() {
+        if (tfod == null) {
+            throw new RuntimeException("tfod must be initialized first");
+        }
+
+        // Create the VisionPortal by using is Builder.
+        visionPortal = new VisionPortal.Builder()
+
+                // Set the camera to the webcam hardware.
+                .setCamera(myOpMode.hardwareMap.get(WebcamName.class, "Webcam 1"))
+
+                // Choose a camera resolution. Not all cameras support all resolutions.
+                //.setCameraResolution(new Size(640, 480))
+
+                // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+                //.enableCameraMonitoring(true)
+
+                // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+                //.setStreamFormat(VisionPortal.StreamFormat.YUY2)
+
+                // Choose whether or not LiveView stops if no processors are enabled.
+                // If set "true", monitor shows solid orange screen if no processors enabled.
+                // If set "false", monitor shows camera view without annotations.
+                //.setAutoStopLiveView(false)
+
+                // Set and enable the processor.
+                .addProcessor(tfod)
+
+                // Build the Vision Portal, using the above settings.
+                .build();
+    }
+
+    /**
+     * Drive robot to the targeted position designated by the passed leftInches and
+     * rightInches, at the power specified by speed.
+     * @param leftInches
+     * @param rightInches
+     * @param speed
+     */
+    public void autoDriveRobot(int leftInches, int rightInches, double speed) {
+        int leftInchesToCPI = (int) (leftInches * COUNTS_PER_INCH);
+        int rightInchesToCPI = (int) (rightInches * COUNTS_PER_INCH);
+
+        int leftFrontTarget = leftFrontWheel.getCurrentPosition() + leftInchesToCPI;
+        int leftRearTarget = leftFrontWheel.getCurrentPosition() + leftInchesToCPI;
+        int rightFrontTarget = leftFrontWheel.getCurrentPosition() + rightInchesToCPI;
+        int rightRearTarget = leftFrontWheel.getCurrentPosition() + rightInchesToCPI;
+
+        leftFrontWheel.setTargetPosition(leftFrontTarget);
+        leftRearWheel.setTargetPosition(leftRearTarget);
+        rightFrontWheel.setTargetPosition(rightFrontTarget);
+        rightRearWheel.setTargetPosition(rightRearTarget);
+
+        setRunModeForAllWheels(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //Set up telemetry
+        myOpMode.telemetry.setAutoClear(false);
+        myOpMode.telemetry.addData("Heading", "Current Wheel Positions");
+        Telemetry.Item leftFrontWheelItem = myOpMode.telemetry.addData("LF Wheel", leftFrontWheel.getCurrentPosition());
+        Telemetry.Item leftRearWheelItem = myOpMode.telemetry.addData("LR Wheel", leftRearWheel.getCurrentPosition());
+        Telemetry.Item rightFrontWheelItem = myOpMode.telemetry.addData("RF Wheel", rightFrontWheel.getCurrentPosition());
+        Telemetry.Item rightRearWheelItem = myOpMode.telemetry.addData("RR Wheel", rightRearWheel.getCurrentPosition());
+        myOpMode.telemetry.update();
+
+        // Power all wheels for as long as they are busy.
+        setPowerAllWheels(speed);
+        while (leftFrontWheel.isBusy() && leftRearWheel.isBusy() && rightFrontWheel.isBusy() && rightRearWheel.isBusy()) {
+            leftFrontWheelItem.setValue(leftFrontWheel.getCurrentPosition());
+            leftRearWheelItem.setValue(leftRearWheel.getCurrentPosition());
+            rightFrontWheelItem.setValue(rightFrontWheel.getCurrentPosition());
+            rightRearWheelItem.setValue(rightRearWheel.getCurrentPosition());
+            myOpMode.telemetry.update();
+        }
+
+        setPowerAllWheels(0); //Whoa.
+        myOpMode.telemetry.setAutoClear(true);
+    }
+
+    /**
+     * autoDriveRobot using DEFAULT_WHEEL_MOTOR_SPEED.
+     * @param leftInches
+     * @param rightInches
+     */
+    public void autoDriveRobot(int leftInches, int rightInches) {
+        autoDriveRobot(leftInches, rightInches, DEFAULT_WHEEL_MOTOR_SPEED);
+    }
+
+    /**
+     * Set the RunMode for all wheel motors to the passed runMode.
+     * @param runMode
+     */
+    public void setRunModeForAllWheels(DcMotor.RunMode runMode) {
+        leftFrontWheel.setMode(runMode);
+        leftRearWheel.setMode(runMode);
+        rightFrontWheel.setMode(runMode);
+        rightRearWheel.setMode(runMode);
+    }
+
+    /**
+     * Set the Power for all wheels to the passed speed.
+     * @param speed
+     */
+    public void setPowerAllWheels(double speed) {
+        double absoluteSpeed = Math.abs(speed);
+        leftFrontWheel.setPower(absoluteSpeed);
+        leftRearWheel.setPower(absoluteSpeed);
+        rightFrontWheel.setPower(absoluteSpeed);
+        rightRearWheel.setPower(absoluteSpeed);
+    }
+
+    public List<Recognition> getFreshTfodRecognitions() {
+        return tfod.getFreshRecognitions();
+    }
+
+    /**
+     * Calculates the left/right motor powers required to achieve the requested
+     * robot motions: Drive (Axial motion) and Turn (Yaw motion).
+     * Then sends these power levels to the motors.
+     *
+     * @param Drive     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
+     * @param Turn      Right/Left turning power (-1.0 to 1.0) +ve is CW
+     */
+/*
+    public void manuallyDriveRobot(double Drive, double Turn) {
+        // Combine drive and turn for blended motion.
+        double left  = Drive + Turn;
+        double right = Drive - Turn;
+
+        // Scale the values so neither exceed +/- 1.0
+        double max = Math.max(Math.abs(left), Math.abs(right));
+        if (max > 1.0)
+        {
+            left /= max;
+            right /= max;
+        }
+
+        // Use existing function to drive both wheels.
+        setDrivePower(left, right);
+    }
+*/
+
+    /**
+     * Pass the requested wheel motor powers to the appropriate hardware drive motors.
+     *
+     * @param leftWheel     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
+     * @param rightWheel    Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
+     */
+/*
+    public void setDrivePower(double leftWheel, double rightWheel) {
+        // Output the values to the motor drives.
+        leftDrive.setPower(leftWheel);
+        rightDrive.setPower(rightWheel);
+    }
+*/
+
+    /**
+     * Pass the requested arm power to the appropriate hardware drive motor
+     *
+     * @param power driving power (-1.0 to 1.0)
+     */
+    public void setArmPower(double power) {
+        armMotor.setPower(power);
+    }
+
+    /**
+     * Send the two hand-servos to opposing (mirrored) positions, based on the passed offset.
+     *
+     * @param offset
+     */
+    public void setHandPositions(double offset) {
+        offset = Range.clip(offset, -0.5, 0.5);
+        leftHand.setPosition(MID_SERVO + offset);
+        rightHand.setPosition(MID_SERVO - offset);
+    }
+}
