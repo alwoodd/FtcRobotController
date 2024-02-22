193d27181c656bd4e470e547cec2623a55f8fa09
Dan Alwood <dalwood@yahoo.com>
2024-02-22 12:36:35 -0600

    Added JavaDoc folder.

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.firstinspires.ftc.teamcode","l":"RobotHardware.HeadingTelemetry"},{"p":"org.firstinspires.ftc.teamcode","l":"RobotHardware"}];updateSearchResults();
