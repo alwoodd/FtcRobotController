9cb7a5f30303b06af595b51ec08943594373e5cf
Dan Alwood <dalwood@yahoo.com>
2024-09-30 15:58:11 -0500

    Added most recent JavaDoc; removed old TFOD code that is no longer supported by FtcRobotController SDK.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -44,12 +44,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.tfod.TfodProcessor;
-
-import java.net.InetAddress;
-import java.util.List;
 
 /**
  * Instead of each Op Mode class redefining the robot's hardware resources within its implementation,
@@ -74,7 +69,6 @@ public class RobotHardware {
     private WebcamName webCam;
     private Servo   leftHand;
     private Servo   rightHand;
-    private TfodProcessor tfod;
     private VisionPortal visionPortal;
     private DistanceSensor leftDistanceSensor;
     private DistanceSensor rightDistanceSensor;
@@ -138,8 +132,6 @@ public class RobotHardware {
     public void init() {
         initWheelMotors();
         initServos();
-        initTfod();
-        initVisionPortal();
         initDistanceSensors();
         initColorSensor();
         initAnalogInputs();
@@ -200,69 +192,6 @@ public class RobotHardware {
         leftHand.setPosition(MID_SERVO);
         rightHand.setPosition(MID_SERVO);
     }
-
-    /**
-     * Initialize the TensorFlow Object Detection processor.
-     */
-    private void initTfod() {
-        // Create the TensorFlow processor by using its Builder.
-        tfod = new TfodProcessor.Builder()
-                // Call various setters on the Builder.
-                // Use setModelAssetName() if the TF Model is built in as an asset.
-                // Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-                //.setModelAssetName(TFOD_MODEL_ASSET)
-                //.setModelFileName(TFOD_MODEL_FILE)
-                //.setModelLabels(LABELS)
-                //.setIsModelTensorFlow2(true)
-                //.setIsModelQuantized(true)
-                //.setModelInputSize(300)
-                //.setModelAspectRatio(16.0 / 9.0)
-
-                //Finally, call build() to return a TfodProcessor.
-                .build();
-
-        // Set confidence threshold for TFOD recognitions, at any time.
-        //tfod.setMinResultConfidence(0.75f);
-
-        // Disable or re-enable the TFOD processor at any time.
-        //visionPortal.setProcessorEnabled(tfod, true);
-    }
-
-    /**
-     * Initialize the VisionPortal. tfod is expected to be initialized before calling this method.
-     */
-    private void initVisionPortal() {
-        if (tfod == null) {
-            throw new RuntimeException("tfod must be initialized first");
-        }
-
-        // Create the VisionPortal by using is Builder.
-        visionPortal = new VisionPortal.Builder()
-
-                // Set the camera to the webcam hardware.
-                .setCamera(myOpMode.hardwareMap.get(WebcamName.class, "Webcam 1"))
-
-                // Choose a camera resolution. Not all cameras support all resolutions.
-                //.setCameraResolution(new Size(640, 480))
-
-                // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-                //.enableCameraMonitoring(true)
-
-                // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-                //.setStreamFormat(VisionPortal.StreamFormat.YUY2)
-
-                // Choose whether or not LiveView stops if no processors are enabled.
-                // If set "true", monitor shows solid orange screen if no processors enabled.
-                // If set "false", monitor shows camera view without annotations.
-                //.setAutoStopLiveView(false)
-
-                // Set and enable the processor.
-                .addProcessor(tfod)
-
-                // Build the Vision Portal, using the above settings.
-                .build();
-    }
-
 
     /**
      * Initialize distance sensor(s).
@@ -487,7 +416,8 @@ public class RobotHardware {
      * @param headingSpeedAdjustment - power adjustment applied to baseSpeed to turn the robot to the desiredHeading.
      */
     public void setPowerAllWheels(double baseSpeed, double headingSpeedAdjustment) {
-        /**
+        /*
+        *
          * If your robot uses a negative power to move forward,
          * you'll need to subtract for leftSpeed, and add for
          * rightSpeed.
@@ -688,15 +618,6 @@ public class RobotHardware {
         leftHand.setPosition(MID_SERVO + offset);
         rightHand.setPosition(MID_SERVO - offset);
     }
-
-    /**
-     * Gets a list containing recognitions that were detected since the last call to this method, or null if no new recognitions are available.
-     * @return
-     */
-    public List<Recognition> getFreshTfodRecognitions() {
-        return tfod.getFreshRecognitions();
-    }
-
 
     /**
      * Move XYZ servo so that drone is released.
