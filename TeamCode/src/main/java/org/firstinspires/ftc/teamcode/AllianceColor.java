541ceb6a6479968c032c2261c07dfb80262d7971
Dan Alwood <dalwood@yahoo.com>
2026-03-09 13:49:42 -0500

    Refactored TeamPaths into PedroPather and TeamPoses.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AllianceColor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AllianceColor.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.lhssa.ftc.teamcode.pedroPathing;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
